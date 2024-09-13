15e42e831d86b6930223e8ccb1bcd98d25d10df8
Antonio Augusto de Moura Neto <160650423+AugustoNeto00@users.noreply.github.com>
2024-09-13 18:53:00 +0000

    update

---
+++ aula 4 de poo/funcionario.php
@@ -0,0 +1,13 @@
+<?php
+class funcionario {
+    protected $cpf = 0;
+    protected $email;
+    protected $salario;
+    function __construct($cpf,$formação, $email, $salario){
+        $this->cpf = $cpf;
+        $this->email = $email;
+        $this->salario = $salario;
+        $this->telefono = $formaÃ£o;
+    }
+}
+echo "bpop";
