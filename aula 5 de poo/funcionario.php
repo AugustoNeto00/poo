dcd5647cc7574b873fea60a83edb73c9496a42f7
Antonio Augusto de Moura Neto <160650423+AugustoNeto00@users.noreply.github.com>
2024-09-14 17:35:18 +0000

    update

--- aula 5 de poo/funcionario.php
+++ aula 5 de poo/funcionario.php
@@ -1,90 +1,145 @@
 <?php
-//Uma coisa que jesus me ensinou ↓
-$tabelaTurmas = []; //Digamos que seja um mini banco de dados, serve pra armazenar os alunos cadastrar
-class turma {
-    function __construct($serie = 1, $email = 0, $horario = 2, $sala = 'A'){
-        $this->serie=$serie;
-        $this->email=$email;
-        $this->horario=$horario;
-        $this->sala=$sala;
-        $this->alunos=[];
-    }
-    function getSerie(){
-        return $this->serie;
-    }
-    function getEmail(){
-        return $this->email;
-    }
-    function getHorario(){
-        return $this->$horario;
-    }
-    function getSala(){
-        return $this->sala;
-    }
-    function getAluno(){
-        return $this->alunos;
-    }
-}
-class alunos {
-    protected $email;
-    private $cpf;
-    function __construct($nome = "luquinhas", $cpf = 0, $idade = 12, $email = 0, $matricula = 0){
-        $this->cpf = $cpf;
-        $this->nome = $nome;
-        $this->email = $email;
-        $this->idade=$idade;
-        $this->matricula=$matricula;
-    }
-}
-class funcionario {
-    protected $cpf = 0;
-    protected $email;
-    protected $salario;
-    private $formação;
-    function __construct($nome = "Peido", $cpf = 0 ,$formação = "gay" ,$email = 0 ,$cargo = "presidente" , $salario = 10){ //é possivel criar atributos publicos diretamente do construct, ajuda bastante e economiza linhas
-        $this->cpf = $cpf;
-        $this->nome = $nome;
-        $this->email = $email;
-        $this->salario = $salario;
-        $this->formação = $formação;
-        $this->email= $email;
-        $this->cargo= $cargo;
-    }
-    function getNome(){
-        return $this->nome;
-    }
-    function getCpf(){
-        return $this->cpf;
-    }
-    function getEmail(){
-        return $this->email;
-    }
-    function getSalario(){
-        return $this->salario;
-    }
-    function getFormação(){
-        return $this->cargo;
-    }
-    function baterPonto($horario){
-        echo $this->nome. " bateu o ponto no horário: ". $horario;
-    }
-    function cadastrarAluno($aluno, $turma){
-        $turma->alunos[] = $aluno;
-        echo $aluno. " cadastrado  na turma". $turma->serie;
-    }
-
-}
-class professor extends funcionario {
-    function __construct($nome, $cpf ,$formação ,$email ,$cargo , $salario, $turma){
-        parent::__construct($nome, $cpf ,$formação ,$email ,$cargo , $salario);
-        $this->turma=$turma;
-    }
+class cursos {
+	public $modulos;
+	public $materia;
+	public $media_para_passar;
+	protected $mensalidade;
+	private $matricula;
+	function __construct($modulos = [], $materia = "Lógica de Programação", $media_para_passar = 7.0, $mensalidade = 13.000, $matricula = 0001) {
+		$this->modulos = $modulos;
+		$this->materia = $materia;
+		$this->media_para_passar = $media_para_passar;
+		$this->mensalidade = $mensalidade;
+		$this->matricula = $matricula;
+	}
 }
 
+class Turma {
+	public $serie;
+	public $email;
+	public $horario;
+	public $sala;
+	public $alunos;
 
-//Area dos objts
-$turma1 = new turma();
-$func1 = new funcionario();
+	function __construct($serie = 1, $email = 0, $horario = 2, $sala = "A") {
+		$this->serie = $serie;
+		$this->email = $email;
+		$this->horario = $horario;
+		$this->sala = $sala;
+		$this->alunos = [];
+	}
 
-//Area de mostrar as coisas
-echo $func1->cadastrarAluno("davi", $turma1);
+	function getSerie() {
+		return $this->serie;
+	}
+	function getEmail() {
+		return $this->email;
+	}
+	function getHorario() {
+		return $this->horario;
+	}
+	function getSala() {
+		return $this->sala;
+	}
+	function getAluno() {
+		return $this->alunos;
+	}
+}
+
+class Alunos {
+	protected $email;
+	private $cpf;
+	public $nome;
+	public $idade;
+	public $matricula;
+
+	function __construct($nome = "luquinhas", $cpf = 0, $idade = 12, $email = 0, $matricula = 0) {
+		$this->cpf = $cpf;
+		$this->nome = $nome;
+		$this->email = $email;
+		$this->idade = $idade;
+		$this->matricula = $matricula;
+	}
+
+	function estudar($aluno, $turma) {
+		$turma->alunos[] = $aluno;
+		echo "O aluno: " . $aluno . " da turma: " . $turma->serie . ", está estudando na sala: '" . $turma->sala . "'";
+	}
+	function apresentar($aluno, $turma, $materia, $modulos, $horario) {
+		$turma->alunos[] = $aluno;
+		$cursos->materia = $materia;
+		$cursos->modulos = $modulos;
+		echo "O aluno: ". $aluno. " da turma ". $turma->serie. " terá que apresentar um trabalho de: ". $cursos->materia. " do modulo". $cursos->modulos. " no horário: ". $horario;
+	}
+}
+
+class Funcionario {
+	protected $cpf;
+	protected $email;
+	protected $salario;
+	private $formacao;
+	public $nome;
+	public $cargo;
+
+	function __construct($nome = "Funcionário", $cpf = 0, $formacao = "Professor", $email = 0, $cargo = "presidente", $salario = 10) {
+		$this->cpf = $cpf;
+		$this->nome = $nome;
+		$this->email = $email;
+		$this->salario = $salario;
+		$this->formacao = $formacao;
+		$this->cargo = $cargo;
+	}
+
+	function getNome() {
+		return $this->nome;
+	}
+
+	function getCpf() {
+		return $this->cpf;
+	}
+
+	function getEmail() {
+		return $this->email;
+	}
+
+	function getSalario() {
+		return $this->salario;
+	}
+
+	function getFormacao() {
+		return $this->formacao;
+	}
+
+	function baterPonto($horario) {
+		if ($horario > 13) {
+			echo $this->nome . " bateu o ponto no horário: " . $horario. "da manhã";
+		} else {
+			echo $this->nome . " bateu o ponto no horário: " . $horario. " da tarde";
+		}
+	}
+
+	function cadastrarAluno($aluno, $turma) {
+		$turma->alunos[] = $aluno;
+		echo $aluno . " cadastrado na turma " . $turma->serie;
+	}
+}
+
+class Professor extends Funcionario {
+	function __construct($nome, $cpf, $formacao, $email, $cargo, $salario) {
+		parent::__construct($nome, $cpf, $formacao, $email, $cargo, $salario);
+	}
+}
+
+// Área dos objetos
+$turma1 = new Turma();
+$func1 = new Funcionario();
+$aluno1 = new Alunos();
+$aluno2 = new Alunos();
+$curso1 = new cursos();
+
+// Área de mostrar as coisas
+$func1->cadastrarAluno("Davi", $turma1). "\n". $func1->baterPonto(12);
+echo "\n";
+$aluno1->estudar("Pipo", $turma1);
+$aluno2->apresentar("pedro", "Portugues", $turma1, $curso1, 2, 13);
+echo "\n";
