946af98fdb85cdfb67a9c7d46becc1b44ae9bbc4
Antonio Augusto de Moura Neto <160650423+AugustoNeto00@users.noreply.github.com>
2024-09-26 18:47:52 +0000

    Uptade, new code lines and i finally made the code to get the orders correctly.

--- Aula6/produtospedidos.php
+++ Aula6/produtospedidos.php
@@ -4,27 +4,45 @@
 #A classe Produto deve ter as propriedades $nome e $preco, além de um método getPreco().
 #A classe Pedido deve conter uma lista de produtos e um método adicionarProduto() para adicionar um produto ao pedido.
 #A classe Pedido deve ter um método calcularTotal() para calcular o valor total dos produtos no pedido.
-class Products{
-    private $name;
-    private $price;
-    function __construct($name, $price){
-        $this->name = $name;
-        $this->price = $price;
+class Products {
+	private $name;
+	private $price;
+	function __construct($name, $price) {
+		$this->name = $name;
+		$this->price = $price;
+	}
+	function getPrice() {
+		return $this->price;
+	}
+	function getName() {
+		return $this->name;
+	}
 }
-    function getPreco($price){
+class Order {
+	private $product = [];
+	function getName() {
+		if (!empty($this->product)) {
+			return $this->product[0]->getName();
+		} else {
+			echo "Don't have products in there...";
+		}
+	}
+    function getPrice(){
         return $this->price;
-}
-}
-class Order{
-    private $products = [];//Here, i try to do a array to put the products, but i don't even know how to do that but i'm tryin'.
-    function __construct(Products $products){
-        $this->products = $products;
     }
-    function addProducts($productadd){ //Put the damn arg here!! 
-        $productadd = $this->products->$name;
-        echo $productadd;
+    
+	//Here, i do a array to put the products, but i don't even know how to do that but i'm trying.
+	function addProducts(products $product) {
+		//Put the damn arg here!!
+		$this->product[] = $product;
+		print_r("You have put a product: ".$this->getName(). " in your cart.");
+	}
+    function calcPrice($pricet){
+        $pricet = $this->getPrice() + $this->getPrice();
+        return $pricet / $pricet;
     }
 }
-$products1 = new products("Maça", 12);
-$order1 = new order("maça", 30);
-echo $order1->addProducts( $products1);
+
+$teste = new Products("maça", 12);
+$cliente = new Order();
+$cliente->addProducts($teste). $cliente->calcPrice();
